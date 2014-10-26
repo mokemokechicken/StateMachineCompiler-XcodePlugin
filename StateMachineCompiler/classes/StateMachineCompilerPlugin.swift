d9298f8f493723cf54459cd86b07e7fe9b369412
Ken Morishita <k_morishita@yumemi.co.jp>
2014-10-26 11:37:37 +0900

    temp

--- StateMachineCompiler/classes/StateMachineCompilerPlugin.swift
+++ StateMachineCompiler/classes/StateMachineCompilerPlugin.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import AppKit
+
+private let API_BASE_URL = "http://goodparts.d.yumemi.jp"
 
 private var instance_: StateMachineCompilerPlugin?
 
@@ -27,10 +30,110 @@ public class StateMachineCompilerPlugin : NSObject {
     }
     
     func setup() {
+        var editMenu_ = NSApp.mainMenu??.itemWithTitle("Edit")?.submenu
+        var itemIndex : Int = editMenu_?.indexOfItemWithTitle("Refactor") ?? -1
+        if let editMenu = editMenu_ {
+            if itemIndex >= 0 {
+                let smcMenuItem = NSMenuItem(title: "Generate SMC", action: "generateSMC:", keyEquivalent: "")
+                smcMenuItem.target = self
+                editMenu.insertItem(smcMenuItem, atIndex: itemIndex)
+            }
+        }
+    }
+    
+    func generateSMC(sender: AnyObject) {
+        NSLog("generateSMC Called")
+        if let url = currentFileURL() {
+            NSLog("generateSMC Called: \(url)")
+            if url.pathExtension != "sm" {
+                return
+            }
+            if let smc = NSData(contentsOfURL: url) {
+                let api = SMCAPI.Generate(baseUrl: API_BASE_URL)
+                api.request(.Swift, smc: smc) { response in
+                    if response.isSuccess() {
+                        if let body = response.data {
+                            if let content = NSString(data: body, encoding: NSUTF8StringEncoding) {
+                                NSLog(content)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func currentFileURL() -> NSURL? {
+        if let currentWindowController = NSApp.keyWindow??.windowController() as? NSWindowController {
+            if currentWindowController.isKindOfClass(NSClassFromString("IDEWorkspaceWindowController")) {
+                if let editor: AnyObject = currentWindowController.valueForKeyPath("editorArea.lastActiveEditorContext.editor") {
+                    var document : AnyObject?
+                    if editor.isKindOfClass(NSClassFromString("IDESourceCodeEditor")) {
+                        document = editor.valueForKey("sourceCodeDocument")
+                    } else if editor.isKindOfClass(NSClassFromString("IDESourceCodeComparisonEditor")) {
+                        if let primaryDocument: AnyObject = editor.valueForKey("primaryDocument") {
+                            if primaryDocument.isKindOfClass(NSClassFromString("IDESourceCodeDocument")) {
+                                document = primaryDocument
+                            }
+                        }
+                    }
+                    if let doc: AnyObject = document {
+                        let fileReferences = doc.valueForKey("knownFileReferences") as? [AnyObject]
+                        return fileReferences?.first?.valueForKeyPath("resolvedFilePath.fileURL") as? NSURL
+                    }
+                }
+                
+            }
 
+        }
+        return nil
+    }
+}
+
+
+
+class SMCAPI {
+    class Base {
+        let baseUrl: String
+        
+        init(baseUrl: String) {
+            self.baseUrl = baseUrl
+        }
+    }
+    
+    struct Response {
+        var response: NSURLResponse?
+        var data: NSData?
+        var error: NSError?
+        
+        func isSuccess() -> Bool {
+            return error == nil
+        }
+    }
+    
+    enum Language : String {
+        case Swift="swift"
+    }
+    
+    class Generate : Base {
+        func request(lang: Language, smc: NSData, callback: (Response) -> Void) {
+            let url = NSURL(string: baseUrl + "/generator/generate")!
+            let request = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            request.setValue("application/smc", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = smc
+            request.setValue("\(smc.length)", forHTTPHeaderField: "Content-Length")
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (res, data, error) in
+                let r  = Response(response: res, data: data, error: error)
+                callback(r)
+            }
+        }
     }
 
 }
+
+
+
 
 /*
 public class PluginUtils : NSObject {
